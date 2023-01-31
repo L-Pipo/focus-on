312c8b635600031faa97b56b67e4798ead2ba262
leapipo <lea.deborah.pipo@gmail.com>
2023-01-31 15:45:37 +0100

    --amend

--- server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-// import { Router, Response, Request } from "express";
-
-// export const indexRouter = Router();
-
-// indexRouter.get("/", function (req: Request, res: Response) {
-//   res.send({ title: "Express" });
-// });
